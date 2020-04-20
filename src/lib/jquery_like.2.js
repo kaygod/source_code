2afe508c5e9b23c8161ffbbab68a6a8e57142c4e
kaygod <184641801@qq.com>
2020-04-20 20:15:39 +0800

    实现jquery选择器

--- src/lib/jquery_like.2.js
+++ src/lib/jquery_like.2.js
@@ -9,22 +9,43 @@
   jQuery.fn = jQuery.prototype = {
       init:function(content,context){
         context = context || document;
-        if(typeof context === "string"){
-
-            if(context.startsWith("<") && context.endsWith(">") && context.length>2){ //创建dom节点
-
-                
-
+        this.context = context;
+        if(typeof content === "string"){
+            let child_nodes = [];
+            if(content.startsWith("<") && content.endsWith(">") && content.length>2){ //创建dom节点
+                const DIV = context.createElement("DIV");
+                DIV.innerHTML = content;
+                child_nodes = DIV.children;
             }else{ //寻找dom节点
-
+                child_nodes = context.querySelectorAll(content);
             }
-
-        }else if(context.nodeType){ //判断context是不是node节点
-
-        }else if(typeof context === "function"){//判端context是不是函数
+            jQuery.fn.merge(this,child_nodes);
+            return this;
+        }else if(content.nodeType){ //判断context是不是node节点
+            jQuery.fn.merge(this,[content]);
+            return this;
+        }else if(typeof content === "function"){//判端context是不是函数
 
         }
-
+      },
+      merge:function(array1,array2){
+        if(array1 instanceof Array){
+            return array1.concat(array2);
+        }else{
+            const length = array1.length || 0;
+            i = length;
+            for(j=0;j<array2.length;j++){
+                array1[i+j] = array2[j];
+            }
+            array1.length = length+array2.length;
+            return array1;
+        }  
+      },
+      ready:function(fn){
+         this[0].addEventListener('DOMContentLoaded', function () {
+            document.removeEventListener('DOMContentLoaded', arguments.callee, false);            
+            fn();
+        }, false);
       } 
   }
 
